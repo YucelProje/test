f1a2372d29e5a017e49e4221c0c819263c556200
Yücel ŞAHİN <yucelsahin@comu.edu.tr>
2017-10-02 14:54:17 +0300

    açıklama satırları silindi

--- info.php
+++ info.php
@@ -1,26 +1,5 @@
 <?php
 
 phpinfo();
-/*
-echo "Mevcut:".date("Y-m-d H:i:s");
-echo "<br>";
-date_default_timezone_set("Europe/Moscow");
-echo "Europe/Moscow:".date("Y-m-d H:i:s");
-echo "<br>";
 
-
-echo timezone_version_get();
-/*
-
-$timestamp = time();
-    echo "<p>Timestamp: $timestamp</p>";
-
-    // This returns the correct time
-    echo "<p>". date("Y-m-d H:i:s", $timestamp) ."</p>";
-
-    echo "Set timezone = " . date_default_timezone_set("Atlantic/Stanley");
-
-    // This returns a time 5 hours in the past
-    echo "<p>". date("Y-m-d H:i:s", $timestamp) ."</p>";
-	
-	*/
+?>
